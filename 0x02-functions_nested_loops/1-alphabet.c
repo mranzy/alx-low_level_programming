571b5db747a599b337d7d19d007e16fbfd91f3a3
Anas El Moussaoui <elmoussaouianas@gmail.com>
2023-02-21 14:11:39 -0800

    t

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints characters from a to z
+ * Description: yes indeed
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+_putchar(i);
+_putchar('\n');
+}
