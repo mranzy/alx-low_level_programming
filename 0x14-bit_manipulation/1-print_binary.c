45c56e827433ab4ab8d2e2b9321ac27ef1d225a5
Anas El Moussaoui <elmoussaouianas@gmail.com>
2023-05-04 07:26:03 -0700

    t

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - prints binary of a number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int con;
+	int val = 0;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (con = 1UL << 63; con > 0; con >>= 1)
+		{
+			if (n & con)
+			{
+				_putchar('1');
+				val = 1;
+			}
+			else if (val)
+				_putchar('0');
+		}
+	}
+}
